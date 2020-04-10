1438f7e26884f6ba48c0ea22772365cf7c4ddd51
Andrei Vasilev <andreisvasilev@gmail.com>
2020-04-09 19:25:26 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
     Screen screen;
 
     while(!screen.quit_program()) {
-        Terrain terrain(513, 0.3);          // Generate new terrain map, size must be power of 2
+        Terrain terrain(513, 0.3);          // Generate new terrain map, size must be power of 2 plus 1.
         screen.update(terrain.get_map());   // Update SDL window with new map
     }
 
